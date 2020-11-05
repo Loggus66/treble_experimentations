7ed7d019e76969057b7e22b2476ae935b3a1f3ab
Loggus66 <46504103+Loggus66@users.noreply.github.com>
2020-11-06 03:18:31 +0500

    Update build-dakkar.sh
    Curl no longer finds old patches.zip, oldest row with "curl https://api.github.com/repos/phhusson/treble_experimentations/releases |jq -r '.[] | .tag_name' " is v204. However, it is still available via the direct link. I propose fixing it at its latest version for 8.1, it is unlikely there will be more patches for that branch.

--- build-dakkar.sh
+++ build-dakkar.sh
@@ -396,11 +396,13 @@ download_patches() {
 		githubMatch=v2..
 	elif [[ $localManifestBranch == android-9.0 ]];then
 		githubMatch=v1..
+	elif [[ $localManifestBranch == android-8.1 ]];then
+		githubMatch=v32
 	else
 		githubMatch=v..
 	fi
     jq --help > /dev/null
-	wantedRelease="$(curl --silent https://api.github.com/repos/phhusson/treble_experimentations/releases |jq -r '.[] | .tag_name' |grep -E "$githubMatch\$" |sort -V | tail -n 1)"
+	wantedRelease="$(curl --silent https://api.github.com/repos/phhusson/treble_experimentations/releases |jq -r '.[] | .tag_name' |grep -E "$githubMatch\$" || echo $githubMatch |sort -V | tail -n 1)"
 	wget "https://github.com/phhusson/treble_experimentations/releases/download/$wantedRelease/patches.zip" -O patches.zip
 	rm -Rf patches
 	unzip patches.zip -d patches
